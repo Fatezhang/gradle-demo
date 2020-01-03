f925d4c02d64eb575d849e8714bcac2556e212c0
Fatezhang <762344938@qq.com>
2020-01-03 15:52:36 +0800

    :shipit: Add heartbeat endpoints.

--- scaffold-business/scaffold-business-system/src/main/java/com/cms/scaffold/micro/controller/diagnostic/DiagnosticController.java
+++ scaffold-business/scaffold-business-system/src/main/java/com/cms/scaffold/micro/controller/diagnostic/DiagnosticController.java
@@ -1,4 +1,37 @@
 package com.cms.scaffold.micro.controller.diagnostic;
 
+import com.cms.scaffold.common.Common;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/diagnostic")
 public class DiagnosticController {
+
+    @GetMapping("/")
+    public Map<String, List<String>> index() {
+        Common build = Common.builder().build();
+        Map<String, List<String>> content = new HashMap<>();
+        List<String> list = new ArrayList<>();
+        list.add("/diagnostic/status/heartbeat");
+        list.add("/diagnostic/status/diagnosis");
+        content.put("endpoints", list);
+        return content;
+    }
+
+    @GetMapping("/status/heartbeat")
+    public String heartbeat() {
+        return "OK";
+    }
+
+    @GetMapping("/status/diagnosis")
+    public String diagnosis() {
+        return "OK";
+    }
 }
--- scaffold-core/scaffold-core-common/src/main/java/com/cms/scaffold/common/Common.java
+++ scaffold-core/scaffold-core-common/src/main/java/com/cms/scaffold/common/Common.java
@@ -8,5 +8,6 @@ import lombok.Setter;
 @Setter
 @Builder
 public class Common {
+
     private String name;
 }
